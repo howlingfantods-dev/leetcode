05692b89751e95a4d561e75d137d4799295665f3
Erwin <erwin.b@arketa.com>
2026-05-22 02:18:06 -0500

    Time: 0 ms (100%), Space: 19.4 MB (39.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -6,15 +6,14 @@ class Solution:
             m = l + (r - l) //2
             if nums[m] == target:
                 return m
-
             if nums[r] > nums[m]:
                 if nums[m] <= target <= nums[r]:
                     l = m + 1
                 else:
-                    r = m-1
+                    r = m
             else:
                 if nums[l] <= target <= nums[m]:
-                    r = m-1
+                    r = m
                 else:
                     l = m + 1
         return -1
