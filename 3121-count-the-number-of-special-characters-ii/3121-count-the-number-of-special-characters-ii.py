37c1de33324d52df7f2353529d3dac7d60ee77c6
Erwin <erwin.b@arketa.com>
2026-05-26 19:41:59 -0500

    Time: 975 ms (5.3%), Space: 21.7 MB (15.88%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        c = [0] * 26
+        n = len(word)
+        ans = 0
+        for i in range(n):
+            diff = ord(word[i]) 
+            if word[i].islower():
+                diff -= ord('a')
+                if c[diff] < 0: continue
+                if c[diff] == 0: c[diff]+=1
+                if c[diff] == 1: continue
+                if c[diff] == 2:
+                    ans-=1
+                    c[diff]= -1
+
+            if word[i].isupper():
+                diff -= ord('A')
+                print(word[i],diff,c[diff])
+                if c[diff] < 0: continue
+                if c[diff] == 0:
+                    c[diff] = -1
+                    continue
+                if c[diff] != 1: continue
+                c[diff] = 2
+                ans +=1
+        return ans
+
+                
