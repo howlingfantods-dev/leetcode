6ac5a586444a3f2515134c36cb4ae636c4c9d25b
Erwin <erwin.b@arketa.com>
2026-05-26 20:04:23 -0500

    Time: 56 ms (96.47%), Space: 21.5 MB (62.94%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -1,26 +1,7 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        def dfs(i,c):
-            if i == len(word):
-                return 0
-
-            res = 0
-            diff = ord(word[i]) 
-            if word[i].islower():
-                diff -= ord('a')
-                if c[diff] == 0: c[diff]+=1
-                if c[diff] == 2:
-                    c[diff]= -1
-                    res-=1
-
-            if word[i].isupper():
-                diff -= ord('A')
-                if c[diff] == 0: c[diff] = -1
-                if c[diff] == 1:
-                    c[diff] = 2
-                    res +=1
-
-            return res + dfs(i+1,c)
-
-        return dfs(0,[0] * 26)
-                
+        ans = 0
+        for ch,CH in zip(string.ascii_lowercase,string.ascii_uppercase):
+            if ch in word and CH in word: 
+                ans += word.rfind(ch) < word.find(CH)
+        return ans
