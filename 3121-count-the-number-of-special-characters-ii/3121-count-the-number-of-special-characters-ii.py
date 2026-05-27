f464484952b0313bddfb046344bb9520e81fc3e8
Erwin <erwin.b@arketa.com>
2026-05-26 20:01:09 -0500

    Time: 583 ms (6.47%), Space: 63.1 MB (5.29%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -1,10 +1,9 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        c = [0] * 26
-        n = len(word)
-        def dfs(i):
-            if i == n:
+        def dfs(i,c):
+            if i == len(word):
                 return 0
+
             res = 0
             diff = ord(word[i]) 
             if word[i].islower():
@@ -21,7 +20,7 @@ class Solution:
                     c[diff] = 2
                     res +=1
 
-            return res + dfs(i+1)
+            return res + dfs(i+1,c)
 
-        return dfs(0)
+        return dfs(0,[0] * 26)
                 
