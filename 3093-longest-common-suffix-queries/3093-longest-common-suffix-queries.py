a7524e9be1624b9ccf60fb165148141e13be20f2
Erwin <erwin.b@arketa.com>
2026-05-28 22:36:07 -0500

    Time: 1301 ms (60.55%), Space: 180.3 MB (41.11%) - LeetHub

--- 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.py
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.py
@@ -1,51 +1,50 @@
-class Trie:
+class Node:
     def __init__(self):
-        self.levels = []
-
-    def insert(self, word): 
-        for i in range(len(word)):
-            if i + 1 >  len(self.levels):
-                self.levels.append([0 for _ in range(26)])
-            node = self.levels[i]
-            char = word[len(word)-i-1]
-            asc = ord(char) - ord('a')
-            if node[asc] == 0:
-                node[asc]+=1
-
-    def lcs(word,query):
-        L = len(self.levels) - 1
-        qw = len(word) - 1
-        for qi in range(len(query)-1,-1,-1):
-            if qw == -1: break
-            if query[qi] != word[qw]: break
-            if self.levels[L][ord(query[qi]) - ord('a')] == 0: break
-            qw-=1
-        return len(word) - 1 - qw
-            
-            
+        self.children = {}
+        self.idx = -1
+        self.length = inf
 
 class Solution:
     def stringIndices(self, wordsContainer: List[str], wordsQuery: List[str]) -> List[int]:
-        root = [{}, -1, float('inf')] 
 
-        def consider(node, length, idx):
-            if length < node[2] or (length == node[2] and idx < node[1]):
-                node[2], node[1] = length, idx
+        root = Node()
+        def make(node,idx,length):
+            if (length < node.length
+                or (length == node.length and idx < node.idx)):
+                node.length = length
+                node.idx = idx
 
-        for idx, w in enumerate(wordsContainer):
-            consider(root, len(w), idx)
+        for i, w in enumerate(wordsContainer):
+            make(root, i, len(w))
             node = root
             for ch in reversed(w):
-                node = node[0].setdefault(ch, [{}, -1, float('inf')])
-                consider(node, len(w), idx)
+                if ch not in node.children:
+                    node.children[ch] = Node()
+                node = node.children[ch]
+                make(node,i, len(w))
 
         res = []
+
         for q in wordsQuery:
             node = root
             for ch in reversed(q):
-                if ch in node[0]:
-                    node = node[0][ch]
-                else:
-                    break
-            res.append(node[1])
+                if ch in node.children:
+                    node = node.children[ch]
+                else: break
+            res.append(node.idx)
+
         return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
