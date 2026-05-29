3627d9c87ecccb3f2b3c8bf53661521e9d6a029f
Erwin <erwin.b@arketa.com>
2026-05-28 19:46:54 -0500

    Time: 3 ms (80.35%), Space: 19.5 MB (21.61%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
@@ -1,6 +1,4 @@
 class Solution:
     def minElement(self, nums: List[int]) -> int:
-        def dfs(n):
-            if n == 0: return 0
-            return n % 10 + dfs(n//10)
+        dfs = lambda n: 0 if n == 0 else n%10 + dfs(n//10)
         return min(dfs(n) for n in nums)
